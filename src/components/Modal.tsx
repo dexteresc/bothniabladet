977f886812af5957325b703bf155098c866e33f3
dexteresc <jonathan.jeding@gmail.com>
2022-04-01 18:49:08 +0200

    fix(aria): add types to button
    also added overflow-hidden to body when modal is active

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,36 +1,46 @@
 // Modal
 
+import { useEffect, ReactNode } from "react";
+
 interface ModalProps {
-  children: React.ReactNode;
+  children: ReactNode;
   isOpen: boolean;
   onClose: () => void;
   title?: string;
   className?: string;
 }
 
-const Modal = ({ children, isOpen, onClose, title, className }: ModalProps) => {
-  console.log(onClose);
-  return (
-    <>
-      {isOpen && (
-        <div className="fixed inset-0 z-40 flex items-center justify-center">
-          <div className="fixed inset-0 bg-gray-900 opacity-50" onClick={onClose}></div>
-          <div className="relative">
-            <header>
-              {title && <h1 className="text-2xl">{title}</h1>}
-              {/* Close button */}
-              <button className="absolute top-0 right-0 m-4" onClick={onClose}>
-                <span className="material-icons">close</span>
-              </button>
-            </header>
-            <main className={`${className} bg-white dark:bg-gray-700 rounded-lg px-2 pb-2 pt-4 z-50`}>
-              {children}
-            </main>
-          </div>
-        </div>
-      )}
-    </>
-  );
-};
+function Modal({ children, isOpen, onClose, title, className }: ModalProps) {
+  useEffect(() => {
+    document.body.classList.toggle("overflow-hidden", isOpen);
+  });
+  return isOpen ? (
+    <div className="fixed inset-0 z-40 flex items-center justify-center">
+      <div
+        className="fixed inset-0 bg-gray-900 opacity-50"
+        onClick={onClose}
+        onKeyDown={(e) => e.key === "esc" && onClose}
+        role="none"
+      />
+      <div className="relative">
+        <header>
+          {title && <h1 className="text-2xl">{title}</h1>}
+          <button
+            type="button"
+            className="absolute top-0 right-0 m-4"
+            onClick={onClose}
+          >
+            <span className="material-icons">close</span>
+          </button>
+        </header>
+        <main
+          className={`${className} bg-white dark:bg-gray-700 rounded-lg px-2 pb-2 pt-4 z-50`}
+        >
+          {children}
+        </main>
+      </div>
+    </div>
+  ) : null;
+}
 
 export default Modal;
