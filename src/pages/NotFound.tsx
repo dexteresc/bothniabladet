b5739c92885bd79f8cffb343945acaf4838f406d
dexteresc <jonathan.jeding@gmail.com>
2022-04-01 18:47:49 +0200

    fix: improved notfound comp

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,14 +1,22 @@
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 
-const NotFound = () => {
+function NotFound() {
   const navigate = useNavigate();
   return (
     <main className="h-screen w-screen flex flex-col items-center justify-center">
-      <p className="mb-8 font-semibold text-2xl">There's nothing here 😢</p>
-      <button className="rounded px-4 py-2 bg-gray-100 dark:bg-gray-700 flex items-center" onClick={() => navigate(-1)}>
+      <p className="mb-8 font-semibold text-2xl">
+        There&apos;s nothing here{" "}
+        <span className="animate-bounce inline-block">😢</span>
+      </p>
+      <button
+        type="button"
+        className="rounded px-4 py-2 bg-gray-100 dark:bg-gray-700 flex items-center"
+        onClick={() => navigate(-1)}
+      >
         <span className="material-icons mr-2">arrow_back</span>
-         Go back</button>
+        Go back
+      </button>
     </main>
   );
-};
+}
 export default NotFound;
