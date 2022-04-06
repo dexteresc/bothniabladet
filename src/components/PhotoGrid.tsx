27470a67f6d9c24514c903307062a7f8991cb9d6
dexteresc <jonathan.jeding@gmail.com>
2022-04-07 00:42:50 +0200

    fix: add photo-grid comp

---
+++ src/components/PhotoGrid.tsx
@@ -0,0 +1,26 @@
+import { Photo } from "@/api/photo";
+
+function PhotoItem({ photo }: { photo: Photo }) {
+  return (
+    <div className="photo-grid__item">
+      <img src={photo.url} alt={photo.title} />
+    </div>
+  );
+}
+function PhotoGrid({ photos }: { photos: Photo[] }) {
+  return (
+    <div className="">
+      {photos.length > 0 ? (
+        <div className="p-2 grid grid-flow-col grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {photos.map((photo) => (
+            <PhotoItem key={photo.id} photo={photo} />
+          ))}
+        </div>
+      ) : (
+        <div className="text-center p-4 border border-gray-500 rounded">No photos found</div>
+      )}
+    </div>
+  );
+}
+
+export default PhotoGrid;
