d5a345b404e66a1c4d7ed86dc8419a3871f96965
dexteresc <jonathan.jeding@gmail.com>
2022-04-07 00:38:29 +0200

    fix: rule changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,50 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: ["plugin:react/recommended", "airbnb", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "module"
   },
   plugins: ["react", "@typescript-eslint"],
   globals: {
     React: true,
     JSX: true,
-    shallow: true,
+    shallow: true
   },
   rules: {
     "react/require-default-props": "off",
     "react/jsx-filename-extension": [
       2,
-      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] }
     ],
     "import/no-unresolved": [
       2,
       {
-        ignore: ["^@?/", "^\\.?"],
-      },
+        ignore: ["^@?/", "^\\.?"]
+      }
     ],
     "import/extensions": "off",
     "import/no-cycle": [
       2,
       {
-        maxDepth: 1,
-      },
+        maxDepth: 1
+      }
     ],
     "jsx-a11y/click-events-have-key-events": "off",
     "jsx-a11y/no-noninteractive-element-interactions": "off",
     "jsx-a11y/label-has-associated-control": "off",
-  },
+    "react/jsx-no-bind": [
+      2, {
+        allowArrowFunctions: true,
+        allowFunctions: true,
+      }
+    ]
+  }
 };
