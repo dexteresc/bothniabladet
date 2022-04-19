021401f7d735afa2191f69b1ab3811beb4fb58b0
dexteresc <jonathan.jeding@gmail.com>
2022-04-19 10:47:49 +0200

    fix: move to flex layout

--- src/components/PhotoGrid.tsx
+++ src/components/PhotoGrid.tsx
@@ -1,26 +1,36 @@
 import { Photo } from "@/api/photo";
+import { Icon } from "./Navbar";
 
 function PhotoItem({ photo }: { photo: Photo }) {
   return (
-    <div className="photo-grid__item">
-      <img src={photo.url} alt={photo.title} />
+    <div className="rounded-lg bg-gray-200 dark:bg-gray-700 relative z-0 m-2">
+      <img
+        src={`http://localhost:8080${photo.url}`}
+        alt={photo.title}
+        className="max-h-64 h-auto max-w-full peer rounded-lg"
+      />
+      <div className="flex justify-end absolute p-1 right-0 top-0 left-0 w-full rounded-t-lg bg-gray-900 transition-opacity opacity-0 hover:opacity-50 peer-hover:opacity-50">
+        <a
+          href={`http://localhost:8080${photo.url}/download`}
+          download={photo.title}
+          className="hover:text-white"
+        >
+          <Icon value="get_app" className="text-inherit" />
+        </a>
+      </div>
     </div>
   );
 }
 function PhotoGrid({ photos, text }: { photos: Photo[]; text?: string }) {
-  return (
-    <div className="">
-      {photos.length > 0 ? (
-        <div className="p-2 grid grid-flow-col grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-          {photos.map((photo) => (
-            <PhotoItem key={photo.id} photo={photo} />
-          ))}
-        </div>
-      ) : (
-        <div className="text-center p-4 border border-gray-500 rounded">
-          {text ?? "No photos found"}
-        </div>
-      )}
+  return photos.length > 0 ? (
+    <div className="-mx-2 flex flex-wrap">
+      {photos.map((photo) => (
+        <PhotoItem key={photo.id} photo={photo} />
+      ))}
+    </div>
+  ) : (
+    <div className="text-center p-4 border border-gray-500 rounded">
+      {text ?? "No photos found"}
     </div>
   );
 }
