2d047defb99e0d6767017f829f2c29c662bee778
dexteresc <jonathan.jeding@gmail.com>
2022-04-25 14:51:45 +0200

    fix: formatting & eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
@@ -6,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  }
 });
