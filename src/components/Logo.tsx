9712b3dc1fd8f1f6c0383732e87393067b703611
dexteresc <jonathan.jeding@gmail.com>
2022-04-25 14:59:32 +0200

    fix: add logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ function Logo({ className }: { className?: string }) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 353.75 202.23"
-      className={`${className} text-black dark:text-white`}
+      className={className}
       version="1"
     >
       <title>BothniaBladet logo</title>
