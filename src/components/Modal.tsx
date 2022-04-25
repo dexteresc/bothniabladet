f20e527d10903d6319ab4dbe1e44edb1c496c491
dexteresc <jonathan.jeding@gmail.com>
2022-04-25 14:59:22 +0200

    fix(modal): header background

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -22,22 +22,18 @@ function Modal({ children, isOpen, onClose, title, className }: ModalProps) {
         onKeyDown={(e) => e.key === "esc" && onClose}
         role="none"
       />
-      <div className="relative">
-        <header>
+      <div className="relative bg-white dark:bg-gray-700 rounded-lg px-2 pb-2 pt-4 z-50">
+        <header className="mb-4">
           {title && <h1 className="text-2xl">{title}</h1>}
           <button
             type="button"
-            className="absolute top-0 right-0 m-4"
+            className="absolute top-0 right-0 p-4"
             onClick={onClose}
           >
             <span className="material-icons">close</span>
           </button>
         </header>
-        <main
-          className={`${className} bg-white dark:bg-gray-700 rounded-lg px-2 pb-2 pt-4 z-50`}
-        >
-          {children}
-        </main>
+        <main className={className}>{children}</main>
       </div>
     </div>
   ) : null;
