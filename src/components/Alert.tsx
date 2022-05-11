d22c5c1d8011626a4e75ce898113665df22029c7
dexteresc <jonathan.jeding@gmail.com>
2022-05-11 21:04:10 +0200

    fix: add alert component

---
+++ src/components/Alert.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export default function Alert({
+  type,
+  message,
+  timeout = 5000,
+  onClose
+}: {
+  type?: "error" | "success";
+  message: string;
+  timeout?: number;
+  onClose?: () => void;
+}) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  setTimeout(() => {
+    setIsOpen(false);
+    onClose?.();
+  }, timeout);
+  if (type === "error") {
+    return isOpen ? (
+      <div
+        className="absolute z-40 bottom-4 left-4 lg:left-[21rem] right-4 bg-red-300 text-red-900 rounded p-2"
+        role="alert"
+      >
+        {message}
+      </div>
+    ) : null;
+  }
+  if (type === "success") {
+    return isOpen ? (
+      <div
+        className="absolute z-40 bottom-4 left-4 lg:left-[21rem] right-4 bg-green-300 text-green-900 rounded p-2"
+        role="alert"
+      >
+        {message}
+      </div>
+    ) : null;
+  }
+  return isOpen ? (
+    <div
+      className="absolute z-40 bottom-4 left-4 lg:left-[21rem] right-4 bg-blue-300 text-blue-900 rounded p-2"
+      role="alert"
+    >
+      {message}
+    </div>
+  ) : null;
+}
