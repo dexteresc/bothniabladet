8038d9bffa2ec0ddd8c749ac00e49f365e2b5579
dexteresc <jonathan.jeding@gmail.com>
2022-05-13 16:10:18 +0200

    fix: add className prop to Alert

--- src/contexts/alert.ts
+++ src/contexts/alert.ts
@@ -3,7 +3,7 @@ import { createContext, useContext } from "react";
 interface IAlertContext {
   // prettier-ignore
   // eslint-disable-next-line no-unused-vars
-  addAlert: (type: "error" | "success" | "default", message: string, className: string) => void;
+  addAlert: (type: "error" | "success" | "default", message: string, className?: string) => void;
   // eslint-disable-next-line no-unused-vars
   removeAlert: (index: number) => void;
 }
